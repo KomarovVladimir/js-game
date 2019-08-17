431829fa26f319d00c944d5143c4b58ee275fcc9
Vladimir <komarov2142@gmail.com>
2019-08-17 17:39:17 +1000

    weapon shooting speed

--- src/js/Weapon.js
+++ src/js/Weapon.js
@@ -12,12 +12,26 @@ export default class Weapon {
         this.tileHeight = props.tileHeight;
         this.tilesAmount = props.tilesAmount || 1;
         this.damage = props.damage || 1;
+        this.shotingSpeed = props.shotingSpeed || 1;
+        this.lastShot = null;
         this.speed = props.speed || 1;
         this.weaponX = props.weaponX || 0;
         this.weaponY = props.weaponY || 0;
     }
  
     shot(positionX, positionY, angle) {
+        if (!this.lastShot) {
+            this.lastShot = performance.now(); 
+            this.createBullet(positionX, positionY, angle);
+        }
+        let dt = performance.now() - this.lastShot;
+        if (dt >= 1000 / this.shotingSpeed) {
+            this.lastShot = performance.now();
+            this.createBullet(positionX, positionY, angle);
+        }
+    }
+
+    createBullet(positionX, positionY, angle) {
         objectHandler.createObject(Bullet, {
             group: 'playerBullet',
             image: this.bulletImage,
